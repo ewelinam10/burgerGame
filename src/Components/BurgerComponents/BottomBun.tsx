9fd1d17a54a838a6675c4158941c7d5caa181c8a
Ewelina Mysiak <Ewelina.Mysiak@assecobs.pl>
2022-07-13 10:44:35 +0200

    page animation

--- src/Components/BurgerComponents/BottomBun.tsx
+++ src/Components/BurgerComponents/BottomBun.tsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react';
 import './../../styles/bottomBun.scss'
 const BottomBun = () => {
     const [isWarm, setIsWarmState] = useState<boolean>(true);
-    useEffect(() => {
-        const interval = getIntervalToSetWarmBun(isWarm, setIsWarmState);
-        return () => { clearInterval(interval) };
-    });
+    // useEffect(() => {
+    //     const interval = getIntervalToSetWarmBun(isWarm, setIsWarmState);
+    //     return () => { clearInterval(interval) };
+    // });
     return (
         <div id="bottom-bun">
         </div>
